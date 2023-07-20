7cde9e2454a67d9b0618f267ef0dec40e92fcfa8
Prashant_Som <prashantsom75@gmail.com>
2023-07-20 20:04:27 +0530

    chabge in stats and about

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -8,15 +8,26 @@ const Stats = () => {
     <div className="stats-container">
     <h2 className="skills-heading">My GitHub Stats</h2>
       <div className="stats-content">
-        <div id="github-streak-stats">
+      <div >
+  <a href="https://github.com/0126prashant">
+    <img
+      className="stats-image"
+      id="github-streak-stats"
+      src="https://camo.githubusercontent.com/b940b94c9b1b0a7c2fa882f7efa9d35c94c9420f0019eff409a10e42e5447ada/68747470733a2f2f6769746875622d726561646d652d73747265616b2d73746174732e6865726f6b756170702e636f6d2f3f757365723d303132367072617368616e7426"
+      alt="GitHub Streak Stats"
+    />
+  </a>
+</div>
+        {/* <div id="github-streak-stats">
           <a href="https://github.com/0126prashant">
             <img
               className="stats-image"
               src="https://camo.githubusercontent.com/b940b94c9b1b0a7c2fa882f7efa9d35c94c9420f0019eff409a10e42e5447ada/68747470733a2f2f6769746875622d726561646d652d73747265616b2d73746174732e6865726f6b756170702e636f6d2f3f757365723d303132367072617368616e7426"
+              
               alt="GitHub Streak Stats"
             />
           </a>
-        </div>
+        </div> */}
         <div id="github-stats-card">
           <img
             className="stats-image"
